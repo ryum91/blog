119bf108f30e5c36faa133b7ada2cd1d1e2d4a5a
Charyum.Park <ryum91@gmail.com>
2020-05-02 09:11:30 +0900

    feat: google analytics (#3)

--- src/html.js
+++ src/html.js
@@ -3,8 +3,13 @@ import React from "react";
 export default class HTML extends React.Component {
   render() {
     return (
-      <html lang="en">
+      <html lang="ko">
         <head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-106652933-3"></script>
+          <script dangerouslySetInnerHTML={{
+            __html: 'window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag("js", new Date()); gtag("config", "UA-106652933-3");'
+          }}>
+          </script>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
