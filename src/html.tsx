10bafbd3199c5aee1ca8dff4347c0deefd68bce8
Charyum.Park <ryum91@gmail.com>
2020-12-14 22:56:37 +0900

    style: 폰트 및 스타일 변경

--- src/html.tsx
+++ src/html.tsx
@@ -14,20 +14,15 @@ const HTML = ({ headComponents, body, postBodyComponents }: Props) => {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {headComponents}
-        <link
-          href="/img/apple-touch-icon.png"
-          rel="apple-touch-icon"
-          sizes="180x180"
-        />
+        <link href="/img/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
         <link href="/img/favicon.ico" rel="icon" type="image/x-icon" />
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/moonspam/NanumBarunGothic@latest/nanumbarungothicsubset.css"></link>
+        <link href="http://cdn.jsdelivr.net/gh/joungkyun/font-d2coding/d2coding.css" rel="stylesheet" type="text/css"></link>
       </head>
       <body>
         <div id="___gatsby" dangerouslySetInnerHTML={{ __html: body }} />
         {postBodyComponents}
-        <script
-          async
-          src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        />
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
       </body>
     </html>
   );
